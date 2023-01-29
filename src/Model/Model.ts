f9254d6ffbcbab375b3c84ec571e72b670de7433
Pedro Valério <pedroddjkK@gmail.com>
2023-01-29 18:57:49 -0300

    Iterando sobre Fields e Interface ModelFields

--- src/Model/Model.ts
+++ src/Model/Model.ts
@@ -1,8 +1,16 @@
 import { DbConnection } from "../connection/Connection.js";
 
+interface ModelFields {
+  [key: string]: {
+    type: string;
+    autoIncrement?: boolean;
+    primaryKey?: boolean;
+    allowNull?: boolean;
+  };
+}
 export class Model {
   protected static connection: DbConnection;
-  protected static fields: Object;
+  protected static fields: ModelFields;
   protected static tableName: string;
 
   public static get<T extends Model>(this: { new (): T }, id: string): T {
@@ -14,9 +22,9 @@ export class Model {
   }
 
   public static setup(
-    fields: Object,
+    fields: ModelFields,
     connection: DbConnection,
-    tableName: string,
+    tableName: string
   ): void {
     this.fields = fields;
     this.connection = connection;
@@ -28,6 +36,16 @@ export class Model {
       this.connection.query(
         `CREATE TABLE IF NOT EXISTS ${this.tableName} (id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY (id))`
       );
+      for (const field in this.fields) {
+        const fieldConfig = this.fields[field];
+        const type = fieldConfig.type;
+        const autoIncrement = fieldConfig.autoIncrement ? "AUTO_INCREMENT" : "";
+        const primaryKey = fieldConfig.primaryKey ? "PRIMARY KEY" : "";
+        const allowNull = fieldConfig.allowNull ? "NULL" : "NOT NULL";
+        this.connection.query(
+          `ALTER TABLE ${this.tableName} ADD COLUMN IF NOT EXISTS ${field} ${type} ${autoIncrement} ${primaryKey} ${allowNull}`
+        );
+      }
     } catch (error) {
       console.log("Erro na sincronização: " + error);
     }
