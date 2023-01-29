19cd494cd963e8843e81ef0b43214c7cf611dacd
Pedro Valério <pedroddjkK@gmail.com>
2023-01-29 10:13:08 -0300

    Alterações para o NPM

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
+import { Model } from "./Model/Model";
 import { Connection } from "./connection/Connection.js";
 
-const connection = new Connection();
-
-async function test() {
-  await connection.connect("127.0.0.1", "root", 3306, "fazenda")
-  console.log(connection.isConnected())
-}
-
-test()
+exports.Model = Model;
+exports.Connection = Connection;
