10ed2f2f1ea37df4ca5eb0a11c2adc89f46656bb
Pedro Valério <pedroddjkK@gmail.com>
2023-01-30 22:21:06 -0300

    Atualização nos tipos.

--- src/index.d.ts
+++ src/index.d.ts
@@ -25,4 +25,50 @@ declare module 'ormize' {
 
     public closeConnection(): void;
   }
+
+  interface ModelFields {
+    [key: string]: {
+      type: string;
+      autoIncrement?: boolean;
+      primaryKey?: boolean;
+      allowNull?: boolean;
+    };
+  }
+  
+  export class Model {
+    protected static connection: DbConnection;
+    protected static fields: ModelFields;
+    protected static tableName: string;
+  
+    /**
+     * Recupera um registro a partir de seu id
+     * @param id Id do registro
+     * @returns Instância da classe com o registro
+     */
+    public static get<T extends Model>(this: { new (): T }, id: string): T;
+  
+    /**
+     * Recupera todos os registros da tabela
+     * @returns Array de instâncias da classe
+     */
+    public static getAll<T extends Model>(this: { new (): T }): T[];
+  
+    /**
+     * Configura a conexão com o banco de dados, os campos da tabela e o nome da tabela
+     * @param fields Campos da tabela
+     * @param connection Conexão com o banco de dados
+     * @param tableName Nome da tabela
+     */
+    public static setup(
+      fields: ModelFields,
+      connection: DbConnection,
+      tableName: string
+    ): void;
+  
+    /**
+     * Sincroniza as alterações realizadas no model com o banco de dados
+     * @returns Resultado da operação de sincronização
+     */
+    public static async sync(): Promise<Object>;
+  }
 }
